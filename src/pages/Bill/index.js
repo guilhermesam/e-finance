4380e1c32ab2e5a78c662d343be31f2f5fd8d733
Thalis Antunes <thalisantunes@hotmail.com>
2020-06-11 17:01:29 -0300

    Página de Conta

--- src/pages/Bill/index.js
+++ src/pages/Bill/index.js
@@ -11,39 +11,55 @@ export default function Bill () {
     return (
         <View style={styles.container}>
         <View style={styles.headerContainer}>
+       
+       <Text>A</Text>
         </View>
                
 <View style={styles.form}>
     <Text style={styles.billTitle}>Nome do estabelecimento:</Text>
      <TextInput style={styles.input} placeholder="Insira um nome" placeholderTextColor='#999' autoCapitalize="words" autoCorrect={false}/>
 
-    <Text style={styles.billTitle}>tipo de estabelecimento:</Text>
-     <Picker>
-        <Picker.Item label="Esolha um tipo" value="0" />
+    <Text style={styles.billTitle}>Tipo de estabelecimento:</Text>
+     <Picker style={styles.billPicker}>
+        <Picker.Item label="Selecione uma opção" color="#874e84" value="0" />
         <Picker.Item label="tipo 1" value="1" />
         <Picker.Item label="tipo 2" value="2" />
          </Picker>
     
     <Text style={styles.billTitle}>Data de compra:</Text>
-    <DatePicker style={{width: 200}} date={() => []} mode="date" placeholder="Selecione uma data" format="DD-MM-YYYY"
+    <DatePicker style={styles.billData} date={() => []} mode="date" placeholder="Selecione uma data" format="DD-MM-YYYY"
      minDate="01-01-2016" maxDate="01-01-2019" confirmBtnText="Confirm" cancelBtnText="Cancel"/>
                 
     <Text style={styles.billTitle}>Data de entrega:</Text>
-    <DatePicker style={{width: 200}} date={() => []}  mode="date"  placeholder="Selecione uma data" format="DD-MM-YYYY"
+    <DatePicker style={styles.billData} date={() => []}  mode="date"  placeholder="Selecione uma data" format="DD-MM-YYYY"
     minDate="01-01-2016" maxDate="01-01-2019" confirmBtnText="Confirm" cancelBtnText="Cancel"/>
     
     <Text style={styles.billTitle}>Valor:</Text>
      <TextInput style={styles.input} placeholder="Insira um Valor" placeholderTextColor='#999' autoCapitalize="words" autoCorrect={false}/>
     
+
+
+
+
+
+
+<View style={styles.formButton}>
 <TouchableOpacity style={styles.ButtonConfirm} onPress={() => {}}>
     <Text style={styles.textButton}>Salvar</Text>
          </TouchableOpacity>
+             
              <TouchableOpacity style={styles.ButtonCancel} onPress={() => {}}>
                 <Text style={styles.textButton}>Cancelar</Text>
                  </TouchableOpacity>
-           
+                 
+                 </View>
+                 
+                 
+                 
+                 
+                 </View>
                         </View>
-                            </View>
+                            
     )
 }
 
--- src/pages/Bill/styles.js
+++ src/pages/Bill/styles.js
@@ -1,54 +1,110 @@
 import {StyleSheet} from 'react-native';
+import Fonts from '@expo-google-fonts/roboto';
 
 
 export default StyleSheet.create ({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#006DFF'
+       
+        backgroundColor: '#612f74',
+        
     },
 
     headerContainer: {
         flexDirection: 'row', 
         alignSelf: 'stretch',
         justifyContent: 'center',
+        height: 20
      }, 
 
      form: {
         alignSelf: 'stretch',
         marginHorizontal: 30,
         borderRadius: 16,
-        backgroundColor: '#FFF',
+        backgroundColor: '#f8f8f8',
         height: 550,
-        marginTop: 50,
+        margin: 50,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        
     },
 
    
     billTitle : {
-      marginBottom: 2,
-    
-         
-         fontWeight: 'bold',
+    marginBottom: 2,
+    marginVertical: 17,
+    marginLeft: 10,
+    color: '#874e84',
+    fontFamily: 'Roboto'
          
     },
 
 input : {
-    marginBottom: 2
+    marginBottom: 2,
+    backgroundColor: '#fff',
+    height: 35,
+    width: 250,
+    borderRadius: 10,
+    marginLeft: 15,
 },
 
-    ButtonConfirm:{
-backgroundColor: '#237100'
-    },
+billPicker:{
+    marginLeft: 10, 
+    borderRadius: 10, 
+},
 
-    ButtonCancel:{
-        backgroundColor: '#710300'
+billData : {
+    marginLeft: 10,
+    height: 35,
+    width: 289,
+    borderRadius: 10,
+},
+
+formButton : {
+marginTop: 30,
+    justifyContent: "space-between",
+    flexDirection: 'row',
+    marginHorizontal: 10
+},
+
+
+
+ButtonConfirm:{
+backgroundColor: '#4cd964',
+justifyContent: 'space-between',
+flexDirection: 'row',
+alignItems: 'center',
+height: 40,
+width: 130,
+borderRadius: 10,
+overflow: "hidden",
+marginTop: 8,
+
+},
+
+ButtonCancel:{
+backgroundColor: '#ff3b30',
+justifyContent: 'space-between',
+flexDirection: 'row',
+alignItems: 'center',
+height: 40,
+width: 130,
+borderRadius: 10,
+overflow: "hidden",
+marginTop: 8,
+        
     },
     
     textButton : {
         color: '#ffffff',
-        fontWeight: 'bold'
+     
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        textAlign: "center",
+    
+        fontSize: 16,
     }
 
 
